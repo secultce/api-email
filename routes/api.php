201970991ae862e408c5d21bede83908e8d7c7ac
Junior-Shyko <franciscoanto@gmail.com>
2024-03-20 23:31:37 -0300

    :technologist: Marcando consumidor

--- routes/api.php
+++ routes/api.php
@@ -7,20 +7,42 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
+use PhpAmqpLib\Exchange\AMQPExchangeType;
+
+function process_message($message)
+{
+    echo "\n--------\n";
+    echo $message->body;
+    echo "\n--------\n";
+
+//    $message->ack();
+
+    // Send a message with the string "quit" to cancel the consumer.
+    if ($message->body === 'quit') {
+        $message->getChannel()->basic_cancel($message->getConsumerTag());
+    }
+}
 
 Route::get('send', function() {
-    $exchange = 'email';
-    $queue = 'envioEmailNfe';
+    $exchange = 'router';
+    $queue = 'msgs';
+    $consumerTag = 'consumer';
 
     $connection = new AMQPStreamConnection('rabbitmq', '5672', 'mqadmin', 'Admin123XX_', '/');
     $channel = $connection->channel();
-    $channel->exchange_declare($exchange, false, true, false, false);
+    $channel->queue_declare($queue, false, true, false, false);
 
-    $rabbitMsg = new AMQPMessage('Enviou Will');
-    $channel->basic_publish($rabbitMsg, $exchange, $queue);
-    $channel->close();
-    $connection->close();
+    $channel->exchange_declare($exchange, 'direct', false, true, false);
+
+    $channel->queue_bind($queue, $exchange);
+
+    $channel->basic_consume($queue, $consumerTag, false, false, false, false, 'process_message');
+    $channel->wait();
+//    $channel->close();
+//    $connection->close();
 });
+
+
 
 Route::post('/registrando', [UserController::class, 'store'])
     ->middleware('guest')
